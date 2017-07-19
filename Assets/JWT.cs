58b4a1de9cfea0a1b917c3556c03b650b9249c27
Mauricio Cardozo <loloop@live.com>
2017-07-19 18:09:06 -0300

    limpeza no código

--- Assets/JWT.cs
+++ Assets/JWT.cs
@@ -4,29 +4,30 @@ using System.Security.Cryptography;
 using System.Text;
 
 namespace Rowbots.Crypto{
+	// some bits based on Levitkon's answer on https://stackoverflow.com/questions/10055158/is-there-a-json-web-token-jwt-example-in-c
 	public class JWT {
 
-		const string headerJSON = "{\"alg\":\"HS256\",\"typ\":\"JWT\"}";
+	const string headerJSON = "{\"alg\":\"HS256\",\"typ\":\"JWT\"}";
 
 	static string ToBase64URL(string str){
 		str = System.Convert.ToBase64String(Encoding.UTF8.GetBytes(str));
-		str = str.Split('=')[0]; // Remove any trailing '='s
-		return str;
+		return Base64Escape(str);
 	}
 
-	static string ToHMAC(string str, string secret){	
-		byte[] arrayToComputeFromString = Encoding.UTF8.GetBytes(str);			
-		HMACSHA256 hmac = new HMACSHA256(Encoding.UTF8.GetBytes(secret));
-		byte[] computedHash = hmac.ComputeHash(arrayToComputeFromString);
-		return System.Convert.ToBase64String(computedHash).Replace("=",string.Empty).Replace("/","_").Replace("+","_"); //These Replaces are for conformity with JWT spec
+	static string Base64Escape(string str){
+		return str.Replace("=", string.Empty).Replace("/","_").Replace("+","-");
+	}
+
+	static string ToHMAC(string str, string secret){
+		byte[] computedHash;
+		using (HMACSHA256 hmac = new HMACSHA256(Encoding.UTF8.GetBytes(secret))){
+			computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(str));
+		};
+		return Base64Escape(System.Convert.ToBase64String(computedHash));
 	}
 
 	public static string GenerateToken(object payload, string secret){		
-		string header = ToBase64URL(headerJSON);				
-		string payloadJson = ToBase64URL(JsonUtility.ToJson(payload));
-		string headerload = header + "." + payloadJson;
-		string signature = ToHMAC(str: headerload, secret: secret);
-		return headerload + "." + signature;
+		return GenerateTokenFromString(JsonUtility.ToJson(payload), secret);
 	}
 
 	public static string GenerateTokenFromString(string payload, string secret){		
